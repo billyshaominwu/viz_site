dfc4d3cb9a33458a5437dad551fe8d050597fb2e
Billy Wu <billyw@cs.washington.edu>
2020-02-13 10:26:56 -0800

    demo

---
+++ precache-manifest.6fa10a1b26bd3fe221954bcaeacab1ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1bfd7b4447e0bbf43e1e580c9e3ea3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70dceb98fe089a006b89",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f5b95b9779a5ee6fe8ee",
+    "url": "./static/js/2.ba003c9a.chunk.js"
+  },
+  {
+    "revision": "83d84ef28ed3d22d26926e3b77b52976",
+    "url": "./static/js/2.ba003c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70dceb98fe089a006b89",
+    "url": "./static/js/main.43f7d5b9.chunk.js"
+  },
+  {
+    "revision": "da667d848a0cad6cb2b3",
+    "url": "./static/js/runtime-main.f7fa9f8c.js"
+  },
+  {
+    "revision": "e1d7f378840e8de2bf0ae29014fa4e38",
+    "url": "./static/media/map.e1d7f378.geojson"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d87be654844504e08c3755712fcecccf.js"
+  "./precache-manifest.6fa10a1b26bd3fe221954bcaeacab1ac.js"
 );
 
 self.addEventListener('message', (event) => {
