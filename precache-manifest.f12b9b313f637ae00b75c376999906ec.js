5cea3af041b80c7517e64a07d146a16bee24d641
Billy Wu <billyw@cs.washington.edu>
2020-02-12 23:57:59 -0800

    site build 1

---
+++ precache-manifest.f12b9b313f637ae00b75c376999906ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115bae75c4ce37778467efa405c3e498",
+    "url": "/A3-seattle-crime/index.html"
+  },
+  {
+    "revision": "816435199d0e9ee7bd76",
+    "url": "/A3-seattle-crime/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25a25ba35b105f605c91",
+    "url": "/A3-seattle-crime/static/js/2.49611007.chunk.js"
+  },
+  {
+    "revision": "209ac304699dd1807fccfeda0266d0d2",
+    "url": "/A3-seattle-crime/static/js/2.49611007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816435199d0e9ee7bd76",
+    "url": "/A3-seattle-crime/static/js/main.576ac762.chunk.js"
+  },
+  {
+    "revision": "80d8ece77cf339f76143",
+    "url": "/A3-seattle-crime/static/js/runtime-main.c61cc853.js"
+  },
+  {
+    "revision": "e1d7f378840e8de2bf0ae29014fa4e38",
+    "url": "/A3-seattle-crime/static/media/map.e1d7f378.geojson"
+  }
+]);
